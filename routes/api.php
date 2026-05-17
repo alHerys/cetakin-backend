e50705358ff980bc51ffa2f6457b080d47da9235
crtal7 <crtal735@gmail.com>
2026-05-17 09:55:32 +0700

    feat: add discovery routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Admin\PartnerController as AdminPartnerController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Discovery\DiscoveryController;
+use App\Http\Controllers\Review\ReviewController;
 use App\Http\Controllers\Shop\AtkProductController;
 use App\Http\Controllers\Shop\ShopController;
 use App\Http\Controllers\Shop\ShopPricingController;
@@ -26,6 +28,13 @@ Route::prefix('v1')->group(function () {
         Route::put('/me/services', [ShopServiceController::class, 'update']);
         Route::put('/me/pricing', [ShopPricingController::class, 'update']);
         Route::apiResource('/me/atk', AtkProductController::class)->parameters(['atk' => 'id']);
+    });
+
+    Route::prefix('shops')->middleware(['auth:api', 'role:user'])->group(function () {
+        Route::get('/', [DiscoveryController::class, 'index']);
+        Route::get('/{id}', [DiscoveryController::class, 'show']);
+        Route::get('/{id}/atk', [DiscoveryController::class, 'atkCatalog']);
+        Route::get('/{id}/reviews', [ReviewController::class, 'shopReviews']);
     });
 
     Route::prefix('admin')->middleware(['auth:api', 'role:admin'])->group(function () {
