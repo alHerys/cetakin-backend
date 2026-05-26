a3c8a17460b109383334fcfb720de5767654a841
crtal7 <crtal735@gmail.com>
2026-05-26 18:27:22 +0700

    debug: log PORT value at startup

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+echo "PORT is: ${PORT}"
 touch .env
 
 php artisan migrate --force 2>&1
 
+echo "Starting server on port ${PORT:-8000}..."
 exec php artisan serve --host=0.0.0.0 --port=${PORT:-8000} 2>&1
