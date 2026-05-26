8d061d6cbec761f1fe25b1d038d1f0133c6523d1
crtal7 <crtal735@gmail.com>
2026-05-26 18:31:16 +0700

    debug: log PORT and serve exit code

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
 touch .env
 php artisan migrate --force
-php -S 0.0.0.0:${PORT:-8000} -t public/
+echo "PORT=${PORT}"
+php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
+echo "serve exited with code $?"
